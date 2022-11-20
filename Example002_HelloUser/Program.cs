c2615d85073de216834f4a8fe7f393faa74dd0f9
NazarVik <Nazarov_dev@icloud.com>
2022-11-20 21:20:45 +0300

    fix hello user

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("enter Name");
-string username = Console.Readline();
+Console.Write("enter Name ");
+string username = Console.ReadLine();
 Console.Write("Hello ");
-Console.Write(username);
+Console.WriteLine(username);
