11894db572e60d7d80e6eac83f90bac0fb89e55f
NazarVik <Nazarov_dev@icloud.com>
2022-11-20 22:14:50 +0300

    add if else file

---
+++ Expmple005_if_else/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("ура, это же Маша");
+}
+else
+{
+    Console.Write("Привет,  ");
+    Console.WriteLine("username");
+}
